37ce6e92831bc2ef7852995ef1d74add2f44ee5d
UselessWizard <joshuaharrison111222@gmail.com>
2025-09-27 15:34:08 +1000

    Think i fixed things. why didn't i commit this when i wrote the code...

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
+"""
+Potential experiments within this dataset:
+    - Impact of including/not including hashtags
+    - Experimenting with a number of features of the data:
+        - Message Length
+        - Average word length
+        - Flesch–Kincaid readability level
+"""
+
 import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -16,19 +25,18 @@ data = pd.read_csv("tweets.csv", encoding='latin-1') # tweets.csv has 2 columns,
 # Preprocessing/Data cleaning
 
 data.dropna(subset=["tweet"], inplace=True)
+data = data.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4", "Unnamed: 5"], axis=1)
 
 def data_preprocessing(tweet: str) -> str:
     # Convert to lowercase
     tweet = str(tweet).lower()
-
-    # Remove URLs, hashtags & handles & remove stopwords
     tweet_split = tweet.split(" ")
     tweet_split_processed = []
 
-    # THIS DOESN"T SEEM TO BE WORKING.
-    for word in tweet_split:
-        # Very simple URL detection.  
-        if word not in stopwords.words("english") or not (word[0] == "@" or word[0] == "#"): #and "http" in word and ".com" in word and ".net" in word
+    for word in tweet_split: 
+        # Remove stopwords, handles, hashtags and urls.
+        if word not in stopwords.words("english") and not (word[0] == "@" or word[0] == "#") and \
+            not "http" in word and not (".com" in word or ".net" in word): 
             tweet_split_processed.append(word)
 
     tweet = ""
@@ -36,21 +44,19 @@ def data_preprocessing(tweet: str) -> str:
         tweet = tweet + " " + str(word)
 
     # Remove punctuation
-    #tweet_chars = [char for char in tweet if not (char in string.punctuation or char == "\n")]
+    tweet.replace("\n", " ") # Remove new line characters
+    tweet_chars = [char for char in tweet if not (char in string.punctuation)]
 
-    #tweet = ""
-    #for char in tweet_chars:
-    #    tweet = tweet + str(char)
+    tweet = ""
+    for char in tweet_chars:
+        tweet = tweet + str(char)
     
     # Convert emojis (if applicable).
 
     # Return the processed tweet.
-    print(tweet)
     return tweet
 
-data["tweet"].apply(data_preprocessing)
-
-print(data)
+data["tweet"] = data["tweet"].apply(data_preprocessing)
 
 # Feature Construction - convert to TF-IDF representation
 
@@ -58,7 +64,6 @@ vectorizer = TfidfVectorizer()
 tfidf_data = vectorizer.fit_transform(data["tweet"])
 
 train_data, test_data, train_label, test_label = train_test_split(tfidf_data, data["label"], train_size=0.8, random_state=67)
-print(data["label"].shape)
 
 # Train and test with Naive Bayes
 
